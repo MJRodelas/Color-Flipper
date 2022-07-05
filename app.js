ab27779f95adc7159afed7dffd6683eaa9033602
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-07-05 17:46:52 +0800

    Change colors using all values on the array randomly
    Functions used
    
    Math.random()
    Math.floor()

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const color = document.querySelector(".color");
 btn.addEventListener('click',function(){
 console.log(document.body);
 //get random number between 0 - 3
-    const randonNumber = 2;
+    const randonNumber = getRandomNumber();
+    console.log(randonNumber);
     document.body.style.backgroundColor = colors[randonNumber];
-    color.textContent = colors[randonNumber]
+    color.textContent = colors[randonNumber];
 })
+
+function getRandomNumber(){
+    return Math.floor( Math.random() * colors.length);
+
+}
