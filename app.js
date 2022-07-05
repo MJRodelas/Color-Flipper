9e7f1d4ef9a652027acd3b1114be17cdd496f7ee
MJRodelas <markjaysonrodelas.0521@gmail.com>
2022-07-05 18:19:17 +0800

    Adding documentation to app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]; // array that has values to be used on changing the colors of the page
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
 
-btn.addEventListener('click',function(){
-console.log(document.body);
+const btn = document.getElementById("btn"); //getting the btn on the page to be used on javascript
+const color = document.querySelector(".color"); //getting the color property of the page to be used in javascript
+
+btn.addEventListener('click',function(){ //once you click the button on the page, this function activates
+console.log(document.body); //logging our the body on the console for checking
 //get random number between 0 - 3
-    const randonNumber = getRandomNumber();
-    console.log(randonNumber);
-    document.body.style.backgroundColor = colors[randonNumber];
-    color.textContent = colors[randonNumber];
+    const randonNumber = getRandomNumber(); //transferring the value provided by getRandomNumber() function to the randomNumber variable
+    console.log(randonNumber); // Logging out the value of random color to the console for checking and debugging
+    document.body.style.backgroundColor = colors[randonNumber]; //changes the color of the body of HTML
+    color.textContent = colors[randonNumber]; //changes the value appearing on the page beside the word "Background Color"
 })
 
+//Generate Random Numbers between 0-3. 3 Because there are 4 values on the array 0,1,2,3
+//Array starts with index of 0
 function getRandomNumber(){
     return Math.floor( Math.random() * colors.length);
-
+//Math.floor function removes the decimals
+//Math.random function generates random number 0 to less than 1 (inclusive of 0, but not 1) 
+//Multiplied by the length of colors to have numbers between 0-3
 }
